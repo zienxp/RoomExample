c3afc74c9af5e3c3976a3ae1294576e0aa785036
ahmed.zien <ahmed.zien@gmail.com>
2019-10-10 22:00:31 +0200

    update ...

--- app/src/main/java/com/example/roomexample/MainActivity.java
+++ app/src/main/java/com/example/roomexample/MainActivity.java
@@ -10,5 +10,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (findViewById(R.id.fragment_container)!= null){
+            if(savedInstanceState!=null){
+                return;
+            }
+
+            getSupportFragmentManager().beginTransaction().add(R.id.fragment_container,new HomeFragment()).commit();
+        }
+
     }
+
+
 }
