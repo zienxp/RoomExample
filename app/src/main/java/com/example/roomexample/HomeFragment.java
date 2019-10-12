ceb8e49c404616471062cd2d1b0ada38447bf504
ahmed.zien <ahmed.zien@gmail.com>
2019-10-12 19:59:46 +0200

    update add user fragment

--- app/src/main/java/com/example/roomexample/HomeFragment.java
+++ app/src/main/java/com/example/roomexample/HomeFragment.java
@@ -8,13 +8,14 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class HomeFragment extends Fragment {
-
+public class HomeFragment extends Fragment implements View.OnClickListener {
+private Button addUser;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -25,7 +26,21 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view =inflater.inflate(R.layout.fragment_home, container, false);
+        addUser = view.findViewById(R.id.button_add_user);
+        addUser.setOnClickListener(this);
+        return view;
     }
 
+    @Override
+    public void onClick(View view) {
+
+        switch (view.getId()){
+
+            case R.id.button_add_user:
+               MainActivity.fragmentManager.beginTransaction().replace(R.id.fragment_container,new AddUserFragment()).addToBackStack(null).commit();
+
+        }
+
+    }
 }
--- app/src/main/java/com/example/roomexample/MainActivity.java
+++ app/src/main/java/com/example/roomexample/MainActivity.java
@@ -1,22 +1,24 @@
 package com.example.roomexample;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static FragmentManager fragmentManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        fragmentManager = getSupportFragmentManager();
 
         if (findViewById(R.id.fragment_container)!= null){
             if(savedInstanceState!=null){
                 return;
             }
 
-            getSupportFragmentManager().beginTransaction().add(R.id.fragment_container,new HomeFragment()).commit();
+            fragmentManager.beginTransaction().add(R.id.fragment_container,new HomeFragment()).commit();
         }
 
     }
